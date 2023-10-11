33b3741a114f2f19d2ec2e4ac7e4b2985c9944ff
vlad11 <vlad.lychagin65@yandex.ru>
2023-10-11 17:23:17 +0300

    test

---
+++ SocketTcpServer/Controllers/ServerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketTcpServer.Controllers;
+
+// Серверные операции
+public class ServerController
+{
+
+    public string Pwd() {
+        var path = $"{Environment.CurrentDirectory}\\App_Files";
+        return Directory.Exists(path)
+            ? path
+            : "not found";
+    } // Pwd
+
+} // class ServerController
